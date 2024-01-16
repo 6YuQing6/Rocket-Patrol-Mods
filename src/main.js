b3e4259342a94b11909ccd7d2382540f84dc6eb9
Sunny Han <shan66@ucsc.edu>
2024-01-15 17:02:48 -0800

    first commit

---
+++ src/main.js
@@ -0,0 +1,8 @@
+
+let config = { //object type
+    type: Phaser.AUTO, //type tells phaser what graphic context to use to render the game 
+    width: 640, //game window width
+    height: 480 //game window height
+  }
+
+let game = new Phaser.Game(config) //puts config object into phaser game
